5ac5a58bf69828435eb98956eac617b642d239f1
anerjin <anerjin@gmail.com>
2024-09-06 00:43:06 +0900

    240906_night

--- pageSorce/ui_landing/assets/js/common.js
+++ pageSorce/ui_landing/assets/js/common.js
@@ -3,23 +3,23 @@
  ************************************************************/
 $(document).ready(function() {
 	
-	var app = ".container .appList .appPop"
+	// var app = ".container .appList .appPop"
 	
-	$(app).click(function(){
-		var appName = $(this).attr("data-pop");
+	// $(app).click(function(){
+	// 	var appName = $(this).attr("data-pop");
 		
-		$('#deskWrap').append( "<div id='"+appName+"' class='multiModal'></div>" );
-		$('#deskWrap #'+appName).load('./ui_page/'+appName+'.html');
+	// 	$('#deskWrap').append( "<div id='"+appName+"' class='multiModal'></div>" );
+	// 	$('#deskWrap #'+appName).load('./ui_page/'+appName+'.html');
 		
-		var appClose = "[data-ui='appClose']";
+	// 	var appClose = "[data-ui='appClose']";
 		
-		$("#deskWrap").on("click", appClose, function() {
-			$(this).closest('.multiModal').addClass("hide").delay(280).queue(function(){
-				$(this).remove();
-			})
-		});
+	// 	$("#deskWrap").on("click", appClose, function() {
+	// 		$(this).closest('.multiModal').addClass("hide").delay(280).queue(function(){
+	// 			$(this).remove();
+	// 		})
+	// 	});
 	
-	});
+	// });
 
 });
 
